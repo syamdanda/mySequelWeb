c59678bd81b864e671285621c97d1b09518fa0f8
unknown <syam2011@gmail.com>
2019-01-24 23:40:18 +0530

    deleted unused file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/api');
 
 var config = require('./config');
 

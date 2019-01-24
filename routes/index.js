059a8f9c1884f604545de81f59b6d62ac91cbf26
Syam Danda <syam2011@gmail.com>
2019-01-24 20:25:13 +0530

    added mysql connection poc

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var config = {
+  'host': '',
+  'port':  '',
+  'user': '',
+  'password': '',
+  'database': ''
+}
+var connection;
+
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'MySql Web' });
 });
 
-router.get('/home', function(req, res) {
-  res.render('home', { title: 'MySql Web' });
+router.post('/home', function(req, res) {
+	config.host = req.body.host;
+	config.port = req.body.port;
+	config.user = req.body.user;
+	config.password = req.body.password;
+	config.database = req.body.database;
+	console.log(JSON.stringify(config, null, 2));
+	connection = mysql.createConnection(config);
+	connection.connect();
+	console.log(req.body);
+	connection.query("SELECT table_name FROM information_schema.tables where table_schema='your_database_name'", function (error, results, fields) {
+	  if (error) throw error;
+	  console.log('The solution is: ', results[0].solution);
+	  connection.end();
+	  res.render('home', { title: 'MySql Web' });
+	});	
 });
 
 

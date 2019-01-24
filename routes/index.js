efafdf66ddcf6a0b1e31aace7a87659cc06fe311
unknown <syam2011@gmail.com>
2019-01-24 21:17:16 +0530

    mysql tables reading

--- routes/index.js
+++ routes/index.js
@@ -19,20 +19,26 @@ router.get('/', function(req, res) {
 });
 
 router.post('/home', function(req, res) {
-	config.host = req.body.host;
-	config.port = req.body.port;
-	config.user = req.body.user;
+	config['host'] = req.body['host'];
+	config['port'] = req.body['portNumber'];
+	config.user = req.body.userName;
 	config.password = req.body.password;
-	config.database = req.body.database;
+	config.database = req.body.dbName;
+	console.log(req.body);
 	console.log(JSON.stringify(config, null, 2));
 	connection = mysql.createConnection(config);
 	connection.connect();
-	console.log(req.body);
-	connection.query("SELECT table_name FROM information_schema.tables where table_schema='your_database_name'", function (error, results, fields) {
+	
+	connection.query("SELECT table_name FROM information_schema.tables where table_schema='"+ config.database +"'", function (error, results, fields) {
 	  if (error) throw error;
-	  console.log('The solution is: ', results[0].solution);
+	  console.log(JSON.stringify(results, null, 2));
 	  connection.end();
-	  res.render('home', { title: 'MySql Web' });
+	  var response = {
+	  	'title': 'MySql Web',
+	  	'status': 'SUCCESS',
+	  	'result': results
+	  };
+	  res.render('home', response);
 	});	
 });
 
